eaeffba6bae657fa47f6b371db126ad694d03627
Scot Burrows <scotburrows7962@gmail.com>
2025-03-19 02:46:50 -0400

    Added most of player movement
    Added walking, gravity, crouching, sprinting, stamina, and object collision

--- Assets/TutorialInfo/Scripts/Camera.cs
+++ Assets/TutorialInfo/Scripts/Camera.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
 
-    public Transform orient;
+    public Transform player;
     float xRot;
     float yRot;
+    float xMouse;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,11 +18,22 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        yRot += Input.GetAxisRaw("Mouse X") * Time.deltaTime * 750;
+        xMouse = Input.GetAxis("Mouse X") * Time.deltaTime * 750;
+        yRot += xMouse;
         xRot -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * 750;
 
         xRot = Mathf.Clamp(xRot, -90, 90);
 
         transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        player.Rotate(Vector3.up * xMouse);
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            if (PlayerMovement.crouching) {
+                transform.position += new Vector3(0, -0.2f, 0);
+            }
+            else {
+                transform.position += new Vector3(0, 0.2f, 0);
+            }
+        }
     }
 }
---
+++ Assets/TutorialInfo/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    float moveSpeed = 10f;
+    int stamina = 300;
+    Boolean canSprint = true;
+    public static Boolean crouching = false;
+
+    float fallSpeed = -10f;
+    Vector3 gravity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Sprinting & stamina management
+        if (Input.GetKey(KeyCode.LeftShift) && canSprint) {
+            moveSpeed = 20f;
+            stamina--;
+        }
+        else {
+            moveSpeed = 10f;
+            stamina++;
+        }
+        stamina = Mathf.Clamp(stamina, 0, 300);
+        if (stamina <= 5) {
+            canSprint = false;
+        }
+        else if (stamina >= 295 && !crouching) {
+            canSprint = true;
+        }
+
+        // Crouching
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouching = !crouching;
+        }
+        if (crouching)
+        {
+            canSprint = false;
+            moveSpeed = 5f;
+        }
+
+        //onGround = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - characterController.isGrounded))
+
+        // Movement with WASD
+        Vector3 movement = (Input.GetAxisRaw("Horizontal") * transform.right) + (Input.GetAxisRaw("Vertical") * transform.forward);
+        characterController.Move(movement * Time.deltaTime * moveSpeed);
+
+        // Ground check
+        if (characterController.isGrounded) {
+            fallSpeed = -1f;
+        }
+        else {
+            fallSpeed = -10f;
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            characterController.Move(new Vector3(0.001f, 0, 0));
+            if (characterController.isGrounded)
+            {
+                //fallSpeed = 100f;
+                characterController.Move(new Vector3(0, 10, 0));
+            }
+        }
+
+        // Gravity
+        gravity.y += fallSpeed * Time.deltaTime;
+        characterController.Move(gravity * Time.deltaTime);
+
+    }
+}
