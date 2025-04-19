d3667e23fb6cf835c869d4a330f2b3d172966a65
Scot Burrows <scotburrows7962@gmail.com>
2025-04-19 01:13:46 -0400

    Collecting treasure chests & particle effect
    When you drop a treasure chest into the ship, it is collected and a particle effect plays where you dropped it. Also fixed more stairs collision in the ship.

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    public LayerMask ship;
+    public ParticleSystem collected;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z), 0.1f, ship))
+        {
+            Destroy(Instantiate(collected.gameObject, gameObject.transform.position, gameObject.transform.rotation), 2.0f);
+            Destroy(gameObject);
+        };
+    }
+}
