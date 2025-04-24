65ac099d9857444cac9686becd943e03e3a9496f
Scot Burrows <scotburrows7962@gmail.com>
2025-04-24 03:39:43 -0400

    More UI stuff

---
+++ Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+
+    private Slider slider;
+    private PlayerMovement player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
