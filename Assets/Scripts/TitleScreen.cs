75bfd088fa53861cc27a30973cf3d89714d11cc9
Scot Burrows <scotburrows7962@gmail.com>
2025-05-08 18:13:21 -0400

    Title screen completed

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,25 +1,54 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class TitleScreen : MonoBehaviour
+public class TitleScreen : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    AudioSource sound;
+    public AudioSource sound;
     public AudioClip title1;
     public AudioClip title2;
     //Button
+    public Image button1;
+    public Image button2;
+    Sprite inactive;
+    Sprite active;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Play title screen music
-        sound = GetComponent<AudioSource>();
+        //sound = GetComponent<AudioSource>();
         if (UnityEngine.Random.Range(0f, 2f) >= 1f) sound.clip = title1;
         else sound.clip = title2;
         sound.Play();
+
+        inactive = button1.sprite;
+        active = button2.sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Pirate 2 or Pirate 5
+        
     }
+
+    //Detect if a click occurs
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+        //Debug.Log(name + " Game Object Clicked!");
+        SceneManager.LoadScene("TheWorld");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button1.sprite = active;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        button1.sprite = inactive;
+    }
+
 }
